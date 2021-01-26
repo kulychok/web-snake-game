75f2f643bd565c6488982ebda44ee4d65699ca42
Oleksandr Kulyk <s.v.kulyk55@gmail.com>
2021-01-26 20:01:23 +0000

    Added Poison and queue of field items

--- js/game.js
+++ js/game.js
@@ -4,7 +4,6 @@ const BG_OFFSET = { x: BOX, y: 3 * BOX };
 const SCORE_OFFSET = { x: BOX * 2.5, y: BOX * 1.7 };
 const SNAKE_START = { x: BOX * 9, y: BOX * 10 };
 
-
 const endGameEvent = new Event('endGame');
 
 const game = {
@@ -13,29 +12,30 @@ const game = {
   score: 0,
   items: [],
   ground: new Image(),
+
   init() {
     this.ground.src = "img/ground.png";
-    this.items.push(new Food(), new Snack(), new Mine());
+    this.items.push(new Snack());
     this.setPixelRatio();
   },
+
   run() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.drawImage(this.ground, 0, 0);
     snake.draw(this.ctx);
-
-    for (let item of this.items) {
+    
+    for(let item of this.items) {
       item.draw(this.ctx);
     }
 
     this.showScore();
   },
+
   update() {
     snake.update();
-    for (let item of this.items) {
-      item.update();
-    }
   },
-  setPixelRatio() { //нужно для нормальной плотности точек на канвасе
+
+  setPixelRatio() { 
     let dpr = window.devicePixelRatio || 1;
     let rect = this.canvas.getBoundingClientRect();
     this.canvas.width = rect.width * dpr;
@@ -64,18 +64,19 @@ const game = {
     this.ctx.font = "50px Arial";
     this.ctx.fillText(this.score, SCORE_OFFSET.x, SCORE_OFFSET.y);
   },
-
 };
 
 const snake = {
   tail: [{ x: 9 * BOX, y: 10 * BOX }],
   color: 'green',
+
   draw(ctx) {
     for (const cell of this.tail) {
       ctx.fillStyle = this.color;
       ctx.fillRect(cell.x, cell.y, BOX, BOX);
     }
   },
+
   update() {
     const newHead = { ...this.tail[0] }
     if (this.dir === "left") newHead.x -= BOX;
@@ -85,28 +86,28 @@ const snake = {
     this._checkBorders(newHead);
 
     if(this._hasEatenItself(newHead)) game.end();
-
-    if (!this._hasEatenItem(newHead)) {
+    if (!this._hasEatenItem(newHead)) { 
       this.tail.pop();
     }
     this.tail.unshift(newHead);
-
   },
+
   _checkBorders(head) {
     if ((head.x <= 0 || head.y <= 2 * BOX) || (head.x >= game.canvas.width - BOX || head.y >= game.canvas.height - BOX)) {
       document.dispatchEvent(endGameEvent);
     }
   },
+
   _hasEatenItself(head) {
     if(this.tail.length > 1) {
       for(let item of this.tail) {
         if(head.x === item.x && head.y === item.y)
           return true;
-        
       }
     }
     else return false;
   },
+
   _hasEatenItem(head) {
     for (const item of game.items) {
       if (item.x === head.x && item.y === head.y)
@@ -116,25 +117,62 @@ const snake = {
 }
 
 class Item {
+  static counter = 0;
+
   constructor() {
     this._move();
     this.bonus = 0;
     this.isGrow = false;
     this.img = new Image();
   }
+
   draw(ctx) {
     ctx.drawImage(this.img, this.x, this.y);
   }
-  update() {
-  }
+
   _move() {
-    this.x = Math.floor(Math.random() * NUM_CELLS.width) * BOX + BG_OFFSET.x;
-    this.y = Math.floor(Math.random() * NUM_CELLS.height) * BOX + BG_OFFSET.y;
+    do {
+      this.x = Math.floor(Math.random() * NUM_CELLS.width) * BOX + BG_OFFSET.x;
+      this.y = Math.floor(Math.random() * NUM_CELLS.height) * BOX + BG_OFFSET.y;
+    } while(this._isInSnake(this.x, this.y));
   }
+
+  _isInSnake(x, y) {
+    for(let item of snake.tail) {
+      if(item.x === x && item.y === y) return true;
+    }
+    return false;
+  }
+
   wasEaten() {
     this._move()
     game.score += this.bonus;
+    this._queueChanging();
     return this.isGrow;
+  }
+
+  _queueChanging() {
+    Item.counter++;
+    if(game.items.length > 1) game.items.pop();
+
+    if(Item.counter % 3 === 0) {
+      game.items.pop();
+      game.items.push(new Food);
+      return;
+    }
+
+    if(Item.counter % 5 === 0) {
+      game.items.push(new Mine());
+      return;
+    }
+
+    if(Item.counter % 13 === 0) {
+      game.items.push(new Poison());
+      return;
+    }
+
+    game.items.pop();
+    game.items.push(new Snack());
   }
 }
 
@@ -142,7 +180,7 @@ class Food extends Item {
   constructor() {
     super();
     this.img.src = "img/hamburger.png";
-    this.bonus = 2;
+    this.bonus = 1;
     this.isGrow = true;
   }
 }
@@ -152,7 +190,6 @@ class Snack extends Item {
     super();
     this.img.src = "img/food.png";
     this.bonus = 1;
-    this.isGrow = false;
   }
 }
 
@@ -160,12 +197,22 @@ class Mine extends Item {
   constructor() {
     super();
     this.img.src = "img/mine.png";
-    this.bonus = -1;
+    this.bonus = -5;
   }
 }
 
-document.addEventListener("keydown", direction);
-function direction(event) {
+class Poison extends Item {
+  constructor() {
+    super();
+    this.img.src = "img/poison.png";
+  }
+
+  wasEaten() {
+    game.end();
+  }
+}
+
+document.addEventListener("keydown", () => {
   if (event.key === 'ArrowLeft' && snake.dir !== "right")
     snake.dir = "left";
   if (event.key === 'ArrowRight' && snake.dir !== "left")
@@ -174,10 +221,9 @@ function direction(event) {
     snake.dir = "up";
   if (event.key === 'ArrowDown' && snake.dir !== "up")
     snake.dir = "down";
-}
+});
 
 document.addEventListener('endGame', () => game.end());
 
 game.init();
-
 const gameInterval = setInterval(() => { game.run(); game.update() }, 200);
